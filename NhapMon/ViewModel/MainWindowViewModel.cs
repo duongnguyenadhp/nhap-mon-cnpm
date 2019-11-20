d281e1a5bd65c084369235c12bb24006ea773bbe
duongnguyenadhp <49874960+duongnguyenadhp@users.noreply.github.com>
2019-11-20 23:38:29 +0700

    Thêm ViewModel và Model
    Thêm tầng viewmodel và model cho project

---
+++ NhapMon/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VMHelper;
+namespace ViewModel
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+           
+        }
+    }
+}
---
+++ NhapMon/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class ViewModelLocator
+    {
+        private static MainWindowViewModel _mainWindowViewModelStatic;
+        public static MainWindowViewModel MainWindowViewModelStatic
+        {
+            get
+            {
+                if (_mainWindowViewModelStatic == null)
+                    _mainWindowViewModelStatic = new MainWindowViewModel();
+                return _mainWindowViewModelStatic;
+            }
+        }
+    }
+}
